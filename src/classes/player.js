d19a5b6b35d86133bf3ba22513e21a0c31598d4b
gonnagetbetter <denissss906@gmail.com>
2023-06-07 01:30:33 +0300

    Fix: deleted changedirection method and fixed coords changing

--- src/classes/player.js
+++ src/classes/player.js
@@ -3,36 +3,31 @@ export default class Player {
     this.x = 0;
     this.y = 1;
     this.maze = maze;
-    this.direction = 'right';
   }
-
-  changeDirection(newDirection) {
-    if (this.direction === newDirection) return true;
-    //change sprite direction (newDirection
-  }
-
-  checkCollision() {
-    let nX, nY;
-    switch (this.direction) {
+  checkCollision(direction) {
+    let nX = this.x;
+    let nY = this.y;
+    switch (direction) {
       case 'up':
-        nY = this.y - 1;
+        nY -= 1;
         break;
       case 'down':
-        nY = this.y + 1;
+        nY += 1;
         break;
       case 'left':
-        nX = this.x - 1;
+        nX -= 1;
         break;
       case 'right':
-        nX = this.x + 1;
+        nX += 1;
         break;
     }
-    if (this.maze[nX][nY] === 'false') this.move(nX, nY);
+    if (this.maze[nY][nX] === false) return { isMoveAllowed: true, nY, nX };
+    return false;
   }
 
-  move(x, y) {
-    this.x = x;
+  move(y, x) {
     this.y = y;
-    return { x, y };
+    this.x = x;
+    return { y, x };
   }
 }
