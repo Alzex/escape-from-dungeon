64f0caf4cafd713a4b89b7bcd7e61573156d8127
gonnagetbetter <denissss906@gmail.com>
2023-06-07 14:06:29 +0300

    Feature: added test's when there is no path

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,22 +2,34 @@ import MazeGenerator from '../src/classes/mazeGenerator.js';
 import MazeSolver from '../src/classes/pathFinder.js';
 
 describe('Maze Test', () => {
-  let maze, solver;
+  let labyrinth, solver;
 
   beforeEach(() => {
-    maze = new MazeGenerator(10, 10);
-    solver = new MazeSolver(maze.generate());
+    labyrinth = new MazeGenerator(10, 10);
+    solver = new MazeSolver(labyrinth.generate());
   });
 
   describe('Maze Generator', () => {
     test('should generate a maze', () => {
-      expect(maze.generate()).toBeTruthy();
+      expect(labyrinth.generate()).toBeTruthy();
     });
   });
 
   describe('Path Finder', () => {
     test('should find a path', () => {
       expect(solver.solve()).toBeTruthy();
+    });
+    test('should not find a path', () => {
+      labyrinth.maze[1][1] = true;
+      solver = new MazeSolver(labyrinth.maze);
+      expect(solver.solve()).toBeFalsy();
+    });
+    test('should not find a path', () => {
+      labyrinth.maze[labyrinth.maze.length - 2][
+        labyrinth.maze[0].length - 1
+      ] = true;
+      solver = new MazeSolver(labyrinth.maze);
+      expect(solver.solve()).toBeFalsy();
     });
   });
 });
